2ad0817b6adee66f800b284de9a24ac71218aec3
RAJESH <RAJESH@DESKTOP-JKD0S3K>
2025-12-03 17:31:46 +0530

    My First Commit

---
+++ ProjectForMavenGitHubJankinesPratice/src/test/java/package1/Class1Test.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1Test {
+	@Test
+	public void class1()
+	{
+		Reporter.log("**********Er.Sanjana Thakur*********",true);
+	}
+	
+}
---
+++ ProjectForMavenGitHubJankinesPratice/src/test/java/package1/Class3Test.java
@@ -0,0 +1,14 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class3Test {
+	@Test
+	public void class1()
+	{
+		Reporter.log("**********Er.Sanjana Thakur*********",true);
+	}
+	
+	
+}
